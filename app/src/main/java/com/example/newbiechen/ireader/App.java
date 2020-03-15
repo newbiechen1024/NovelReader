a53170c0951b826999db62696faa241208b07172
newbiechen <newbiechen@gmail.com>
2020-03-15 10:30:05 +0800

    change:forbid DownloadService in order to compatible android O

--- app/src/main/java/com/example/newbiechen/ireader/App.java
+++ app/src/main/java/com/example/newbiechen/ireader/App.java
@@ -3,6 +3,7 @@ package com.example.newbiechen.ireader;
 import android.app.Application;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 
 import com.example.newbiechen.ireader.service.DownloadService;
 import com.squareup.leakcanary.LeakCanary;
@@ -18,7 +19,11 @@ public class App extends Application {
     public void onCreate() {
         super.onCreate();
         sInstance = this;
-        startService(new Intent(getContext(), DownloadService.class));
+
+        // TODO:暂时没空适配高版本
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O) {
+            startService(new Intent(getContext(), DownloadService.class));
+        }
 
         // 初始化内存分析工具
         if (!LeakCanary.isInAnalyzerProcess(this)) {
@@ -26,7 +31,7 @@ public class App extends Application {
         }
     }
 
-    public static Context getContext(){
+    public static Context getContext() {
         return sInstance;
     }
 }
